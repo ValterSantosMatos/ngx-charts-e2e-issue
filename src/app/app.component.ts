9cbc61c3fbe2819e68044a840b5071e51a9c3445
Valter Matos <valter.santos.matos@8securities.com>
2017-10-27 09:21:26 +0100

    added ngx-charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  view: any[] = [700, 400];
+  data: any[];
+
+  constructor() {
+    this.data = [
+      {
+        'name': 'Germany',
+        'value': 8940000
+      },
+      {
+        'name': 'USA',
+        'value': 5000000
+      },
+      {
+        'name': 'France',
+        'value': 7200000
+      }
+    ];
+  }
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C']
+  };
+
+  onSelect(event) {
+    console.log(event);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
